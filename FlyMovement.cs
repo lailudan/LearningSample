34e0c019a3c7b246b9b860d7121dfe832cbde3a2
LUDAN LAI <lailudan@hotmail.com>
2017-11-19 08:19:27 -0500

    Add files via upload

---
+++ FlyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Transform Center;
+
+    private float flySpeed;
+
+	// Use this for initialization
+	void Start () {
+        flySpeed = Random.Range(300f, 700f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Center.position, Vector3.up, flySpeed * Time.deltaTime);	
+	}
+}
