f1aa775bb063de43563b6eb620ab7ab3a6076e17
LUDAN LAI <lailudan@hotmail.com>
2017-11-19 09:05:36 -0500

    Add files via upload

---
+++ FlyPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyPickup : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pickupPrefab;
+
+
+
+    void OnTriggerEnter(Collider other) {
+        
+        // If the Collider other is tagged with "Player"...
+        if (other.CompareTag ("Player")){
+
+
+            //...add the pickup particles...
+            Instantiate(pickupPrefab,transform.position,Quaternion.identity);
+
+            Destroy(gameObject);
+        
+        }
+
+    }
+
+
+}
